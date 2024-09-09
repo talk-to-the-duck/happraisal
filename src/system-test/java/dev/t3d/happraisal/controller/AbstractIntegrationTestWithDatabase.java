c96ca04fb43b669268c6ca060f35153140f9e673
emmanuelquinton <32171111+emmanuelquinton@users.noreply.github.com>
2024-09-09 21:30:08 +0200

    chore(td3-54)- Replace H2 database by Postgresql (#87)
    * chore(td3-54): use postgres instead of h2
    Co-authored-by: Daniele Cremonini <daniele.cremonini@gmail.com>

---
+++ src/system-test/java/dev/t3d/happraisal/controller/AbstractIntegrationTestWithDatabase.java
@@ -0,0 +1,43 @@
+/* (C)2024 */
+package dev.t3d.happraisal.controller;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class AbstractIntegrationTestWithDatabase {
+
+  static DockerComposeContainer<?> dockerComposeContainer = instantiateDockerComposeContainer();
+
+  private static DockerComposeContainer<?> instantiateDockerComposeContainer() {
+    var dockerComposeFileResource =
+        AbstractIntegrationTestWithDatabase.class.getResource("/docker-compose-postgresql.yml");
+    File dockerComposeFile;
+    try {
+      assert dockerComposeFileResource != null;
+      dockerComposeFile = new File(dockerComposeFileResource.toURI());
+    } catch (URISyntaxException e) {
+      throw new RuntimeException(e);
+    }
+    var container =
+        new DockerComposeContainer<>(dockerComposeFile)
+            .withServices("db", "liquibase")
+            .withExposedService("db", 5432);
+
+    container.start();
+    return container;
+  }
+
+  @DynamicPropertySource
+  public static void registerPosgresUrl(DynamicPropertyRegistry dynamicPropertyRegistry) {
+    var postgresExposedPort = dockerComposeContainer.getServicePort("db", 5432);
+    dynamicPropertyRegistry.add(
+        "spring.datasource.url",
+        () ->
+            String.format("jdbc:postgresql://localhost:%s/happraisal_dbname", postgresExposedPort));
+  }
+}
