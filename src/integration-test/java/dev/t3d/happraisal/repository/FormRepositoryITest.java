b8ea772e4dc471cc254f7d2259840fb9c4896e70
Emmanuel <equinton.pro@gmail.com>
2025-02-17 08:43:38 +0100

    Some updates

--- src/integration-test/java/dev/t3d/happraisal/repository/FormRepositoryITest.java
+++ src/integration-test/java/dev/t3d/happraisal/repository/FormRepositoryITest.java
@@ -2,57 +2,56 @@
 package dev.t3d.happraisal.repository;
 
 import dev.t3d.happraisal.AbstractIntegrationTestWithDatabase;
-import dev.t3d.happraisal.entity.*;
-
-import java.util.Set;
-
-import jakarta.persistence.EntityManager;
+import dev.t3d.happraisal.entity.AuditWriterConfiguration;
+import dev.t3d.happraisal.entity.Form;
+import dev.t3d.happraisal.entity.QuestionAnswer;
 import org.assertj.core.api.BDDAssertions;
 import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Set;
 
 @DataJpaTest
 @ActiveProfiles("it")
 @Import(value = {AuditWriterConfiguration.class})
 public class FormRepositoryITest extends AbstractIntegrationTestWithDatabase {
-  @Autowired private FormRepository formRepository;
+    @Autowired
+    private FormRepository formRepository;
 
-  @Test
-  void should_create_form() {
-    // given
-    var questionAnswer =
-        new QuestionAnswer(null, "What do you think about your work this year", null, null);
-    var formToSave = new Form(null, Set.of(questionAnswer));
+    @Test
+    void should_create_form() {
+        // given
+        var questionAnswer =
+                new QuestionAnswer(null, "What do you think about your work this year", null, null);
+        var formToSave = new Form(null, Set.of(questionAnswer));
 
-    // when
-    var actualForm = formRepository.save(formToSave);
+        // when
+        var actualForm = formRepository.save(formToSave);
 
-    // then
-    var idNotNull = new Condition<Form>((Form form) -> form.getId() != null, "form id not null");
-    var asOneQuestion =
-        new Condition<Form>(
-            (Form form) -> form.getQuestions().size() == 1, "form has only one question");
-    BDDAssertions.then(actualForm)
-        .as("Check if a new form has been saved.")
-        .has(idNotNull)
-        .has(asOneQuestion);
-  }
+        // then
+        var idNotNull = new Condition<Form>((Form form) -> form.getId() != null, "form id not null");
+        var asOneQuestion =
+                new Condition<Form>(
+                        (Form form) -> form.getQuestions().size() == 1, "form has only one question");
+        BDDAssertions.then(actualForm)
+                .as("Check if a new form has been saved.")
+                .has(idNotNull)
+                .has(asOneQuestion);
+    }
 
-  @Test
-  @Sql(scripts = {"/sql_scripts/initialize_form.sql"})
-  void should_find_all_form() {
-    // when
-    var actualForms = formRepository.findAll();
+    @Test
+    @Sql(scripts = {"/sql_scripts/initialize_form.sql"})
+    void should_find_all_form() {
+        // when
+        var actualForms = formRepository.findAll();
 
-    // then
-    BDDAssertions.then(actualForms).as("Check if all forms has been found").hasSize(2);
-  }
+        // then
+        BDDAssertions.then(actualForms).as("Check if all forms has been found").hasSize(2);
+    }
 
 }
--- src/main/java/dev/t3d/happraisal/entity/AuditWriterConfiguration.java
+++ src/main/java/dev/t3d/happraisal/entity/AuditWriterConfiguration.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 public class AuditWriterConfiguration {
